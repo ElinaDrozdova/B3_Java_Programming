0a0a55309c3f1e74dddf1825797cfd65c91da2cd
Elina <elina.g.drozdova@gmail.com>
2024-06-05 20:30:30 -0600

    inheritance

---
+++ src/day34_inheritance/person/LocalStudent.java
@@ -0,0 +1,7 @@
+package day34_inheritance.person;
+
+
+
+public class LocalStudent extends Student {
+    String campusAccessNumber;
+}
---
+++ src/day34_inheritance/person/OnlineStudent.java
@@ -0,0 +1,6 @@
+package day34_inheritance.person;
+
+
+public class OnlineStudent extends Student {
+    String zoomLinkAddress;
+}
